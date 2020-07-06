614c5663638b70f258ef2633a91be6f694d433da
Roman Agarkov <agarkovromachka@mail.ru>
2020-07-06 15:31:54 +0300

    add dev branch & connect server

--- server/app.js
+++ server/app.js
@@ -1,12 +1,47 @@
+// const express = require('express');
+// const graphqlHTTP = require('express-graphql');
+// const schema = require('../schema/schema');
+// const mongoose = require('mongoose');
+// const cors = require('cors');
+// const path = require('path')
+
+// const app = express();
+// const PORT = process.env.PORT || 3005;
+
+// mongoose.connect(`mongodb+srv://TestUser:pass123@cluster0-q1pic.mongodb.net/graph-ql-app?retryWrites=true&w=majority`, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// })
+
+// app.use(cors());
+
+// app.use('/graphql', graphqlHTTP({
+//   schema,
+//   graphiql: true,
+// }));
+
+// // app.use(express.static('public'));
+
+// // app.get('*', (req, res) => {
+// //   res.sendFile(path.resolve(__dirname, 'public', 'index.html '))
+// // })
+
+// const dbConnection = mongoose.connection;
+// dbConnection.on('error', err => console.log(`Connection error: ${err}`));
+// dbConnection.once('open', () => console.log(`Connection to DB!`));
+
+// app.listen(PORT, err => {
+//   err ? console.log(err) : console.log('Server started!');
+// });
+
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('../schema/schema');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const path = require('path')
 
 const app = express();
-const PORT = process.env.PORT || 3005;
+const PORT = 3007;
 
 mongoose.connect(`mongodb+srv://TestUser:pass123@cluster0-q1pic.mongodb.net/graph-ql-app?retryWrites=true&w=majority`, {
   useNewUrlParser: true,
@@ -17,19 +52,12 @@ app.use(cors());
 
 app.use('/graphql', graphqlHTTP({
   schema,
-  rootValue: rootResolver,
   graphiql: true,
 }));
 
-app.use(express.static('public'));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'public', 'index.html '))
-})
-
 const dbConnection = mongoose.connection;
 dbConnection.on('error', err => console.log(`Connection error: ${err}`));
-dbConnection.once('open', () => console.log(`Connection to DB!`));
+dbConnection.once('open', () => console.log('Connected to DB!'));
 
 app.listen(PORT, err => {
   err ? console.log(err) : console.log('Server started!');
